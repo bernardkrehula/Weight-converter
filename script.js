c9200c117afe301205c029fb00a6cceeae55e1b0
bernardkrehula <krehula.bernard456@gmail.com>
2025-01-21 19:11:01 +0100

    Error function
    Adding error function if negative number is added

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const num = document.querySelector('.number input');
 const weight = document.querySelector('.weight h3');
+const error = document.querySelector('.error');
 
 num.addEventListener('keypress', (event) => {
     if(event.key === 'Enter'){
-        let calc = num.value * 0.45;
-        weight.innerHTML = calc.toFixed(2);
+        let size = num.value;
+        if(size >= 0) {
+            let calc = size * 0.45;
+            weight.innerHTML = calc.toFixed(2);
+        }
+        else {
+                error.style.display = 'block';
+                setInterval(resetDiv, 3000);
+        }
     }
-})
+});
+const resetDiv = () => {
+    num.value = 'none';
+    error.style.display = 'none';
+}
