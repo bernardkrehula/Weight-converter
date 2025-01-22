4b4bf3b42c09912ad739fa56c7ce8f7bfc80b492
bernardkrehula <krehula.bernard456@gmail.com>
2025-01-22 19:13:54 +0100

    Change in code
    Code is writen with same function but in different way

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-const num = document.querySelector('.number input');
-const weight = document.querySelector('.weight h3');
-const error = document.querySelector('.error');
+const num = document.querySelector(".number input");
+const weight = document.querySelector(".weight h3");
+const div = document.querySelector(".main");
+/*
+const form = document.createElement("form"); //Promijeni div sa class main da bude forma
+form.className = "main";
+form.innerHTML = div.innerHTML;
+div.replaceWith(form);
+*/
+let inputValue; // Napravi varijablu input value
 
-num.addEventListener('keypress', (event) => {
-    if(event.key === 'Enter'){
-        let size = num.value;
-        if(size >= 0) {
-            let calc = size * 0.45;
-            weight.innerHTML = calc.toFixed(2);
-        }
-        else {
-                error.style.display = 'block';
-                setInterval(resetDiv, 3000);
-        }
-    }
+let result; //Napravi varijablu result
+
+num.addEventListener("input", () => {
+  //Dodaj event listener na input i sacuvaj vrijednost inputa u varijablu inputValue
+  inputValue = num.value;
+  console.log(inputValue);
 });
-const resetDiv = () => {
-    num.value = 'none';
-    error.style.display = 'none';
+
+div.addEventListener("submit", (inputValue) => {
+  //Na formu dodaj event listener submit
+  inputValue.preventDefault();
+  convertPoundsToKilogram(); //Unutar nje pozovi funkciju convertPoundsToKilogram koristeci input value kao argument
+});
+
+function convertPoundsToKilogram() {
+  //Napravi funckiju convertPoundsToKilogram
+  if (inputValue >= 0) {
+    result = inputValue * 0.45;
+    weight.innerHTML = result.toFixed(2);
+    console.log(result);
+  }
 }
