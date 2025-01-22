672439223474d8cc996d87a0baa11117c18dd912
bernardkrehula <krehula.bernard456@gmail.com>
2025-01-22 19:38:14 +0100

    Changed code

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const num = document.querySelector(".number input");
-const weight = document.querySelector(".weight h3");
 const div = document.querySelector(".main");
-/*
+
 const form = document.createElement("form"); //Promijeni div sa class main da bude forma
 form.className = "main";
 form.innerHTML = div.innerHTML;
 div.replaceWith(form);
-*/
+
+const num = form.querySelector(".number input");
+const weight = form.querySelector(".weight h3");
+
 let inputValue; // Napravi varijablu input value
 
 let result; //Napravi varijablu result
@@ -14,20 +15,18 @@ let result; //Napravi varijablu result
 num.addEventListener("input", () => {
   //Dodaj event listener na input i sacuvaj vrijednost inputa u varijablu inputValue
   inputValue = num.value;
-  console.log(inputValue);
 });
 
-div.addEventListener("submit", (inputValue) => {
+form.addEventListener("submit", (event) => {
   //Na formu dodaj event listener submit
-  inputValue.preventDefault();
+  event.preventDefault();
   convertPoundsToKilogram(); //Unutar nje pozovi funkciju convertPoundsToKilogram koristeci input value kao argument
 });
 
+//Napravi funckiju convertPoundsToKilogram
 function convertPoundsToKilogram() {
-  //Napravi funckiju convertPoundsToKilogram
   if (inputValue >= 0) {
     result = inputValue * 0.45;
     weight.innerHTML = result.toFixed(2);
-    console.log(result);
   }
 }
